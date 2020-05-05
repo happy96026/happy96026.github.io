0822eadbac10f85db99787f9d4040989c7037919
Min Soung Choi <m.choi960@gmail.com>
2020-05-04 19:36:10 -0600

    changed from css to scss

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 "use strict"
 
 function main () {
-  const navContainer = document.querySelector('.nav-container')
+  var navContainer = document.querySelector('.nav-container')
+  var navButton = document.querySelector('.nav-button')
+  var nav = document.querySelector('.nav')
 
-  window.addEventListener('scroll', e => {
-    if (window.scrollY == 0)
+  var navHeight = window.getComputedStyle(nav).height
+  nav.classList.remove('nav_init')
+
+  window.addEventListener('scroll', function () {
+    if (window.pageYOffset == 0)
       navContainer.classList.add('nav-container_top')
     else
       navContainer.classList.remove('nav-container_top')
+  })
+
+  navButton.addEventListener('click', function () {
+    if (nav.classList.contains('nav_show')) {
+      nav.style.height = ''
+      nav.classList.remove('nav_show')
+    } else  {
+      nav.style.height = navHeight
+      nav.classList.add('nav_show')
+    }
   })
 }
 
